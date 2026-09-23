bd16b7644361979db3b5159df2957fd350a42424
Penny <enderman542@gmail.com>
2026-09-23 20:53:15 +0800

    Add OrderProducer class for sending OrderEvent messages to Kafka

---
+++ order-service/src/main/java/kafka/OrderProducer.java
@@ -0,0 +1,37 @@
+package kafka;
+
+import net.javaguides.basedomains.dto.OrderEvent;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    private NewTopic topic;
+    private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    public OrderProducer(KafkaTemplate<String, OrderEvent> kafkaTemplate, NewTopic topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    public void sendMessage(OrderEvent event){
+        LOGGER.info("Order event => %s", event.toString());
+
+        // create message to send to Kafka topic
+        Message<OrderEvent> message = MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
